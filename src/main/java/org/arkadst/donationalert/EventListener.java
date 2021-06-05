4847ad5127859fe35a43f1f40743d7850e4daf89
Arkadi Statsenko <arkadistatsenko@gmail.com>
2021-06-05 23:42:17 +0300

    unused import

--- src/main/java/org/arkadst/donationalert/EventListener.java
+++ src/main/java/org/arkadst/donationalert/EventListener.java
@@ -6,7 +6,6 @@ import org.bukkit.boss.KeyedBossBar;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Iterator;
 
